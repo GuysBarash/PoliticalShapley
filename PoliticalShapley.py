083768fe6f730fbccfbeeab572119d532c83a7ef
GuyBarsah <guy.barash@wdc.com>
2021-12-24 16:10:28 +0200

    -- aligments

--- PoliticalShapley.py
+++ PoliticalShapley.py
@@ -1,9 +1,27 @@
+import time
+
 import numpy as np
 import pandas as pd
 from IPython.display import display
 import itertools
 import os
+import shutil
 from tqdm import tqdm
+
+
+def clear_folder(path, clear_is_exists=True):
+    if clear_is_exists and os.path.exists(path):
+        all_items_to_remove = [os.path.join(path, f) for f in os.listdir(path)]
+        for item_to_remove in all_items_to_remove:
+            if os.path.exists(item_to_remove) and not os.path.isdir(item_to_remove):
+                os.remove(item_to_remove)
+            else:
+                shutil.rmtree(item_to_remove)
+
+    if not os.path.exists(path):
+        os.makedirs(path, mode=0o777)
+
+    time.sleep(0.1)
 
 
 def powerset(iterable):
@@ -23,6 +41,8 @@ class PoliticalShapley:
 
         self.shapley_values = None
         self.legal_coalitions = None
+
+        self.root_path = os.path.join(os.path.dirname(__file__), 'Dump')
 
     def add_parties(self, parties):
         self.parties.update(parties)
@@ -87,6 +107,14 @@ class PoliticalShapley:
         self.shapley_values = self.shapley_values.sort_values(ascending=False).fillna(0)
         self.legal_coalitions = self.coalitions_mandats.loc[self.coalitions_value['value'] > 0].reset_index(drop=True)
 
+    def get_mandates(self, prty=None):
+        if prty is not None:
+            return self.parties.get(prty, None)
+        else:
+            df = pd.DataFrame(pd.Series(self.parties, name='Mandates'))
+            df = df.sort_values(by='Mandates', ascending=False)
+            return df
+
     def get_possible_govt(self):
         return self.legal_coalitions
 
@@ -96,12 +124,18 @@ class PoliticalShapley:
         else:
             return self.shapley_values.get(party, None)
 
-    def to_csv(self, path=None):
+    def to_csv(self, path=None, title=None):
+        if path is None:
+            path = self.root_path
+        if title is None:
+            title = ''
+        else:
+            title = f'{title}_'
 
-        shap_path = os.path.join(path, 'Shapley.csv')
+        shap_path = os.path.join(path, f'{title}Shapley.csv')
         self.shapley_values.to_csv(shap_path, header=True)
 
-        possible_govt_path = os.path.join(path, 'possible options.csv')
+        possible_govt_path = os.path.join(path, f'{title}possible options.csv')
         self.legal_coalitions.to_csv(possible_govt_path)
 
 
@@ -111,7 +145,7 @@ def get_campagin_tactics(base, prty):
 
     mx_mandates_to_steal = 4
     sr = pd.DataFrame(columns=range(1, mx_mandates_to_steal + 1), index=prts)
-    for foe in tqdm(prts):
+    for foe in tqdm(prts, desc='Calculating attack tactics'):
         mx_mandates_to_steal_t = min(mx_mandates_to_steal, base.parties[foe])
         for mndts_stolen in range(1, mx_mandates_to_steal_t + 1):
             new_scores = base.parties.copy()
@@ -124,45 +158,256 @@ def get_campagin_tactics(base, prty):
             shap_t.add_restrictions(restrictions)
             shap_t.run()
             shaps = shap_t.get_shapley()
+            coalitions = shap_t.get_possible_govt()
+            coalitions_with_prty = coalitions[coalitions[prty] > 0]
+            coalitions_without_prty = coalitions[coalitions[prty] <= 0]
             sr.loc[foe, mndts_stolen] = shap_t.get_shapley(prty)
 
     for i in range(1, mx_mandates_to_steal + 1):
         if sr[i].unique().shape[0] > 1:
-            sr = sr.sort_values(by=i, ascending=False)
+            sr = sr.sort_values(by=list(range(1, mx_mandates_to_steal + 1)[i - 1:]), ascending=False)
             break
-    display(sr)
+    return sr
+
+
+def get_weak_spots(base, prty):
+    prts = [k for k in base.parties.keys() if k != prty]
+    restrictions = base.disagree.copy()
+
+    mx_mandates_to_steal = 4
+    mx_mandates_to_steal = min(mx_mandates_to_steal, base.parties[prty] - 1)
+    sr = pd.DataFrame(columns=range(1, mx_mandates_to_steal + 1), index=prts)
+    for foe in tqdm(prts, desc='Exploring weak spots'):
+        mx_mandates_to_steal_t = min(mx_mandates_to_steal, base.parties[prty] - 1)
+        for mndts_stolen in range(1, mx_mandates_to_steal_t + 1):
+            new_scores = base.parties.copy()
+            new_scores[foe] += mndts_stolen
+            new_scores[prty] -= mndts_stolen
+            new_scores_sr = pd.Series(new_scores)
+
+            shap_t = PoliticalShapley()
+            shap_t.add_parties(new_scores)
+            shap_t.add_restrictions(restrictions)
+            shap_t.run()
+            shaps = shap_t.get_shapley()
+            coalitions = shap_t.get_possible_govt()
+            coalitions_with_prty = coalitions[coalitions[prty] > 0]
+            coalitions_without_prty = coalitions[coalitions[prty] <= 0]
+            sr.loc[foe, mndts_stolen] = shap_t.get_shapley(prty)
+
+            # if foe == 'new_hope':
+            #     shap_t.to_csv(title='likud_attacked')
+
+    for i in range(1, mx_mandates_to_steal + 1):
+        if sr[i].unique().shape[0] > 1:
+            sr = sr.sort_values(by=list(range(1, mx_mandates_to_steal + 1)[i - 1:]), ascending=True)
+            break
+    return sr
+
+
+def which_rule_to_break(base, prty):
+    scores = base.parties.copy()
+    disagree = list()
+
+    for prty_t in base.disagree.keys():
+        if prty_t == prty:
+            disagree += base.disagree[prty_t]
+        else:
+            if prty in base.disagree[prty_t]:
+                disagree += [prty_t]
+
+    df = pd.DataFrame(index=disagree, columns=['Old Shap', 'New Shap', 'Shap gain', 'options with', 'options without'])
+    for disagree_prty in tqdm(disagree, desc='Who is likely to join Likud'):
+        new_disagree = base.disagree.copy()
+        if disagree_prty in new_disagree.get(prty, list()):
+            new_l = [t for t in new_disagree[prty] if t != disagree_prty]
+            new_disagree[prty] = new_l
+        if prty in new_disagree.get(disagree_prty, list()):
+            new_l = [t for t in new_disagree[disagree_prty] if t != prty]
+            new_disagree[disagree_prty] = new_l
+
+        shap_t = PoliticalShapley()
+        shap_t.add_parties(scores)
+        shap_t.add_restrictions(new_disagree)
+        shap_t.run()
+        shaps = shap_t.get_shapley()
+        coalitions = shap_t.get_possible_govt()
+        coalitions_with_prty = coalitions[coalitions[prty] > 0]
+        coalitions_without_prty = coalitions[coalitions[prty] <= 0]
+
+        df.loc[disagree_prty, 'Old Shap'] = base.get_shapley(disagree_prty)
+        df.loc[disagree_prty, 'New Shap'] = shap_t.get_shapley(disagree_prty)
+        df.loc[disagree_prty, 'Shap gain'] = shap_t.get_shapley(disagree_prty) / base.get_shapley(disagree_prty)
+        df.loc[disagree_prty, ['options with', 'options without']] = len(coalitions_with_prty), len(
+            coalitions_without_prty)
+        if disagree_prty in ['new_hope', 'meretz']:
+            shap_t.to_csv(title=f'{disagree_prty}_joined_likud')
+
+    df = df.sort_values(by='Shap gain', ascending=False)
+    return df
+
+
+def who_has_interest_to_attack(base, prty):
+    prts = [k for k in base.parties.keys() if k != prty]
+    restrictions = base.disagree.copy()
+    scores = base.parties.copy()
+    original_shaps = base.get_shapley()
+
+    mx_mandates_to_steal = 4
+    sr = pd.DataFrame(columns=range(1, mx_mandates_to_steal + 1), index=prts)
+    for foe in tqdm(prts, desc='Calculating agendas'):
+        mx_mandates_to_steal_t = min(mx_mandates_to_steal, base.parties[foe])
+        for mndts_stolen in range(1, mx_mandates_to_steal_t + 1):
+            new_scores = base.parties.copy()
+            new_scores[foe] += mndts_stolen
+            new_scores[prty] -= mndts_stolen
+            new_scores_sr = pd.Series(new_scores)
+
+            shap_t = PoliticalShapley()
+            shap_t.add_parties(new_scores)
+            shap_t.add_restrictions(restrictions)
+            shap_t.run()
+            shaps = shap_t.get_shapley()
+            foe_prev_shap = original_shaps[foe]
+            foe_new_shap = shaps[foe]
+            if foe_prev_shap == 0:
+                attack_gain = 1.0
+                if foe_new_shap > 0:
+                    attack_gain = 2.0
+            else:
+                attack_gain = foe_new_shap / foe_prev_shap
+            sr.loc[foe, mndts_stolen] = attack_gain
+
+    for i in range(1, mx_mandates_to_steal + 1):
+        if sr[i].unique().shape[0] > 1:
+            sr = sr.sort_values(by=list(range(1, mx_mandates_to_steal + 1)[i - 1:]), ascending=False)
+            break
+    return sr
+
+
+def dict_to_latex_table(parties, dispute, print_parties=False, print_restrictions=False, exit_on_complete=False):
+    ld = list(parties.items())
+    ld.sort(key=lambda t: -t[1])
+    ld = [(i + 1, t[0].replace('_', ' ').title(), t[1]) for i, t in enumerate(ld)]
+    parties_idx = {t[1]:t[0] for t in ld}
+
+    if print_parties:
+        ld = list(parties.items())
+        ld.sort(key=lambda t: -t[1])
+        ld = [(t[0].replace('_', ' ').title(), t[1]) for t in ld]
+        ld_s = [f'${idx}$ & {t[0]} & ${t[1]}$' for idx, t in enumerate(ld)]
+        s = ' \\\\\n'.join(ld_s)
+
+        q = [str(t[1]) for t in ld]
+        qs = ','.join(q)
+        print(f"(61;{qs})")
+        print(s)
+    if print_restrictions:
+        l = list()
+        for k, v in dispute.items():
+            k_fixed = k.replace('_', ' ').title()
+            for vt in v:
+                vt_fixed = vt.replace('_', ' ').title()
+                t = (k_fixed, vt_fixed)
+                t_r = (vt_fixed, k_fixed)
+                if (t not in l) and (t_r not in l):
+                    l += [t]
+
+        l_s = [f' {t[0]} & {parties_idx[t[0]]} & {t[1]} & {parties_idx[t[1]]}' for idx, t in enumerate(l)]
+        s = ' \\\\\n'.join(l_s)
+        print(s)
+
+        l_s = [f'({parties_idx[t[0]]}, {parties_idx[t[1]]})' for t in l]
+        s = f'({", ".join(l_s)})'
+        print(s)
+
+    if exit_on_complete:
+        exit(0)
 
 
 if __name__ == '__main__':
     prty = dict()
-    prty['likud'] = 29
-    prty['new_hope'] = 14
-    prty['yamina'] = 13
-    prty['yesh_atid'] = 18
-    prty['meshutefet'] = 9
-    prty['shas'] = 8
+    prty['likud'] = 30
+    prty['new_hope'] = 6
+    prty['yamina'] = 7
+    prty['yesh_atid'] = 17
+    prty['meshutefet'] = 6
+    prty['shas'] = 9
     prty['yahadut_ha_tora'] = 7
-    prty['israel_beitenu'] = 6
-    prty['tzionot_datit'] = 5
-    prty['meretz'] = 0
+    prty['israel_beitenu'] = 7
+    prty['tzionot_datit'] = 6
+    prty['meretz'] = 6
     prty['avoda'] = 7
-    prty['kahol_lavan'] = 4
+    prty['kahol_lavan'] = 8
+    prty['raam'] = 4
 
     disagree = dict()
     disagree['likud'] = ['new_hope', 'kahol_lavan', 'israel_beitenu', 'yesh_atid', 'avoda', 'meretz']
-    disagree['meshutefet'] = ['likud', 'tzionot_datit', 'yamina']
+    disagree['meshutefet'] = ['likud', 'tzionot_datit']
     disagree['yesh_atid'] = ['shas', 'yahadut_ha_tora']
 
 if __name__ == '__main__':
+    path_root = os.path.join(os.path.dirname(__file__), 'Dump')
+    clear_folder(path_root)
+
     shap = PoliticalShapley()
     shap.add_parties(prty)
     shap.add_restrictions(disagree)
     shap.run()
+    print("----- Current board ----- ")
+    df = shap.get_mandates()
+    df.to_csv(os.path.join(path_root, 'Mandates.csv'))
+    display(df)
+    print("----- Current Shapley ----- ")
+    df = pd.DataFrame(shap.get_shapley())
+    df.to_csv(os.path.join(path_root, 'Shapley_vals.csv'))
+    display(df)
+    print("----- Coalitions ----- ")
+    df = shap.get_possible_govt()
+    df.to_csv(os.path.join(path_root, 'Coalitions.csv'))
 
-    prty = 'meshutefet'
-    prty_val = shap.get_shapley(prty)
-    print(f"{prty}: {prty_val}")
-    q = get_campagin_tactics(shap, prty)
+    # print("-------------------------")
+    # print("-------------------------")
+    # prty = 'avoda'
+    # prty_val = shap.get_shapley(prty)
+    # print(f"{prty}: {prty_val}")
+    # print(f"---------------------")
+    # print(f'Mandates: {shap.parties[prty]}')
+    # print(f"Shap: {prty_val}")
+    # print(f"---------------------")
+
+    # coals = shap.get_possible_govt()
+    # coals_with = 0
+    # coals_without = len(coals)
+    # if prty in coals.columns:
+    #     coals_with = len(coals[coals[prty] > 0])
+    #     coals_without = len(coals[coals[prty] <= 0])
+    # print(f'Coalitions with party: {coals_with}')
+    # print(f'Coalitions without party: {coals_without}')
+    # print("-------------------------")
+
+    # offense = get_campagin_tactics(shap, prty)
+    # time.sleep(0.2)
+    # print("Value of attack:")
+    # display(offense)
+
+    # print("\n")
+    # defence = get_weak_spots(shap, prty)
+    # time.sleep(0.2)
+    # print("Value of defence:")
+    # display(defence)
+    #
+    # print("\n")
+    # interest_to_attack = who_has_interest_to_attack(shap, prty)
+    # time.sleep(0.2)
+    # print("Likely to attack:")
+    # display(interest_to_attack)
+
+    # print("\n")
+    # rule_to_break = which_rule_to_break(shap, 'likud')
+    # time.sleep(0.2)
+    # print("Most valueable partner for likud:")
+    # display(rule_to_break)
 
     # df = shap.get_possible_govt()
     # if df.shape[0] > 0:
@@ -173,3 +418,6 @@ if __name__ == '__main__':
     # df = shap.shapley_values
     # print(df)
     # shap.to_csv(r'C:\school\PoliticalShapley')
+
+    print("\n\n")
+    print("END OF CODE.")
